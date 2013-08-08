de53b12aa3f041fb074cab6ecf28df2dba01e3a9
tdoherty <timmeedee@gmail.com>
2013-08-08 14:21:20 -0700

    Initial dist subtree commit

---
+++ dist/test.js
@@ -0,0 +1,41 @@
+[
+  {
+    "AuthAccountNum": "9841000048528501",
+    "PostAccountNum": "82545159",
+    "AccountType": "pax",
+    "RoomNum": "8040",
+    "Name": "Summer, Susan",
+    "VipFlag": false,
+    "ChargingPattern": "YY",
+    "GuestDetail": {
+      "GuestId": "",
+      "Level": "",
+      "ArrivalDate": "",
+      "DepartureDate": "",
+      "CreditLimit": ""
+    }
+  },
+  {
+    "AuthAccountNum": "9841000048529001",
+    "PostAccountNum": "82041618",
+    "AccountType": "misc",
+    "RoomNum": null,
+    "Name": "Master Account",
+    "VipFlag": false,
+    "ChargingPattern": "YY",
+    "GuestDetail": {
+      "GuestId": "",
+      "Level": "",
+      "ArrivalDate": "",
+      "DepartureDate": "",
+      "CreditLimit": ""
+    }
+  },
+  {
+    "AuthAccountNum":
+      "9841000048529002", "PostAccountNum": "82041666", "AccountType": "crew", "RoomNum": null, "Name": "Captain Nemo", "VipFlag": false, "ChargingPattern": "YY", "GuestDetail": {"GuestId": "", "Level": "", "ArrivalDate": "", "DepartureDate": "", "CreditLimit": ""}},
+  {"AuthAccountNum": "9841000048529101", "PostAccountNum": "82766614", "AccountType": "pax", "RoomNum": "8040", "Name": "Robinson, Rachel", "VipFlag": false, "ChargingPattern": "YY", "GuestDetail": {"GuestId": "", "Level": "", "ArrivalDate": "", "DepartureDate": "", "CreditLimit": ""}},
+  {"AuthAccountNum": "9841000048529301", "PostAccountNum": "82041618", "AccountType": "pax", "RoomNum": "8042", "Name": "Perez, Jose Maria", "VipFlag": false, "ChargingPattern": "YY", "GuestDetail": {"GuestId": "", "Level": "", "ArrivalDate": "", "DepartureDate": "", "CreditLimit": ""}},
+  {"AuthAccountNum": "9841000048529401", "PostAccountNum": "82336405", "AccountType": "pax", "RoomNum": "8042", "Name": "Perez, Luisa Maria", "VipFlag": false, "ChargingPattern": "NN", "GuestDetail": {"GuestId": "", "Level": "", "ArrivalDate": "", "DepartureDate": "", "CreditLimit": ""}}
+]
+}
